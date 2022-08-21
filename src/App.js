93071ae09b97fa2f221757a70f4e3665fd963803
Shubh <shubh410shekhar@gmail.com>
2022-08-22 01:11:25 +0530

    UpdatePassword Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import { useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+       {!authCtx.isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route> }
+       {authCtx.isLoggedIn && <Route path='/'>
           <UserProfile />
-        </Route>
+        </Route> }
       </Switch>
     </Layout>
   );
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,14 +1,45 @@
+import React, {useRef, useContext} from 'react';
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const authCtx = useContext(AuthContext)
+  const updatePasswordRef = useRef();
+  const confirmPasswordHandler = (e) => {
+    e.preventDefault();
+    const updatedPassword = updatePasswordRef.current.value;
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBTDKhub2HFtP43AtHuJENxi-RF0CS8Yds', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: updatedPassword,
+        returnSecureToken: true
+    }),
+    headers:{ 
+      'Content-Type': 'application/json'
+   }
+    })
+    .then( res => {
+      if(res.ok) {
+        
+
+      } else {
+        return res.json().then(data => {
+          alert(data.error.message);
+        });
+      }
+    }
+    )
+
+  } 
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={confirmPasswordHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={updatePasswordRef}/>
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button type='submit'>Change Password</button>
       </div>
     </form>
   );
